8cc6cc2e9f5c9d45792c37fe476bf92e674e83fc
Matheus <matheus.tba@hotmail.com>
2020-11-17 00:45:19 -0300

    Formulario de contato

--- js/scripts.js
+++ js/scripts.js
@@ -66,4 +66,50 @@
     });
   
   })(jQuery); // End of use strict
+
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML
+    
+    var form = document.getElementById("contactForm");
+    var button = document.getElementById("sendMessageButtonn");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "<span style='color: green; font-weight:500;'>Obrigado!</span>";
+    }
+
+    function error() {
+      status.innerHTML = "<span style='color: red; font-weight:500;'>Houve um problema :/</span>";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
   
